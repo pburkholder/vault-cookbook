ed989acc7c3989315b70cda53b689a38b4e6cb13
Peter Burkholder <pburkholder@chef.io>
2015-07-09 08:58:27 -0400

    moved

--- test/spec/libraries/vault_config_spec.rb
+++ test/spec/libraries/vault_config_spec.rb
@@ -28,37 +28,36 @@ describe Chef::Resource::VaultConfig do
     before do
       recipe = double("Chef::Recipe")
       allow_any_instance_of(Chef::RunContext).to receive(:include_recipe).and_return([recipe])
-
       allow_any_instance_of(Chef::DSL::Recipe).to receive(:chef_vault_item).and_return(
         { 'certificate' => 'foo', 'private_key' => 'bar' }
       )
     end
 
-      it { is_expected.to create_directory('/etc/vault/ssl/certs') }
-      it { is_expected.to create_directory('/etc/vault/ssl/private') }
+    it { is_expected.to create_directory('/etc/vault/ssl/certs') }
+    it { is_expected.to create_directory('/etc/vault/ssl/private') }
 
-      it do
+    it do
         is_expected.to create_file('/home/vault/.vault.json')
         .with(owner: 'vault')
         .with(group: 'vault')
         .with(mode: '0640')
-      end
+    end
 
-      it do
+    it do
         is_expected.to create_file('/etc/vault/ssl/certs/vault.crt')
         .with(content: 'foo')
         .with(owner: 'vault')
         .with(group: 'vault')
         .with(mode: '0644')
-      end
+    end
 
-      it do
+    it do
         is_expected.to create_file('/etc/vault/ssl/private/vault.key')
         .with(content: 'bar')
         .with(sensitive: true)
         .with(owner: 'vault')
         .with(group: 'vault')
         .with(mode: '0640')
-      end
+    end
   end
 end
